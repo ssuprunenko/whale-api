ef30be708a9497228a00ff493afb12fefb596334
Sergey Suprunenko <suprunenko.s@gmail.com>
2015-01-29 14:47:22 +0300

    [feature] SignUp/LogIn

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,40 @@
+module API
+  module V1
+    class AuthController < Grape::API
+      include API::V1::Defaults
+
+      resource :auth do
+        desc 'Sign Up'
+        params do
+          requires :email, type: String
+          requires :password, type: String
+        end
+        post do
+          account = Account.new(permitted_params)
+          if account.save
+            account
+          elsif account.invalid?
+            error!({error: account.errors.messages}, 422)
+          else
+            error!('Unexpected Error', 500)
+          end
+        end
+
+        desc 'Log In'
+        params do
+          requires :email, type: String
+          requires :password, type: String
+        end
+        post :login do
+          account = Account.find_by(email: params[:email].downcase)
+
+          if account && account.valid_password?(params[:password])
+            account
+          else
+            error!('401 Unauthorized', 401)
+          end
+        end
+      end
+    end
+  end
+end
--- app/models/account.rb
+++ app/models/account.rb
@@ -7,7 +7,9 @@ class Account < ActiveRecord::Base
   before_save :ensure_authentication_token
 
   def ensure_authentication_token
-    self.authentication_token ||= generate_authentication_token
+    if authentication_token.blank?
+      self.authentication_token = generate_authentication_token
+    end
   end
 
   private
