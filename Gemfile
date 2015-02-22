2a659cc344d917c6b8926cadf780c76a0a6f032b
Sergey Suprunenko <suprunenko.s@gmail.com>
2015-02-22 18:51:12 +0300

    [cleanup] unnecessary Rails-API gem

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'rails', '4.2.0'
-gem 'rails-api'
 
 # JSON API
 gem 'grape'
--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,6 +1,6 @@
 Instagram.configure do |config|
-  config.access_token = ENV['INSTAGRAM_ACCESS_TOKEN']
-  config.client_id = ENV['INSTAGRAM_CLIENT_ID']
+  config.access_token  = ENV['INSTAGRAM_ACCESS_TOKEN']
+  config.client_id     = ENV['INSTAGRAM_CLIENT_ID']
   config.client_secret = ENV['INSTAGRAM_CLIENT_SECRET']
-  config.redirect_uri = ENV['INSTAGRAM_REDIRECT_URI']
+  config.redirect_uri  = ENV['INSTAGRAM_REDIRECT_URI']
 end
